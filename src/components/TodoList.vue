4598c04350124fdb57bdbead8ce861c4881075ed
kay <aogora2431@gmail.com>
2018-06-02 19:16:51 +0900

    컴포넌트 기본 구조 코딩 완료

---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>list</div>
+</template>
+<script>
+export default {
+
+  data() {
+    return {
+    };
+  },
+  created() {
+  },
+  methods: {
+  },
+};
+</script>
+<style>
+
+</style>
