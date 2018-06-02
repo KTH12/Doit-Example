22b4e528e5c1331ba4ff0ef64830135ff933efe5
kay <aogora2431@gmail.com>
2018-06-02 19:50:39 +0900

    1. 149페이지까지 완료

--- src/App.vue
+++ src/App.vue
@@ -23,8 +23,19 @@ export default {
 }
 </script>
 
-<style lang="scss">
-  #app {
-    color:black;
+<style >
+  body {
+    text-align: center;
+    background-color: #F6F6F8;
+  }
+  input {
+    border-style: groove;
+    width: 200px;
+  }
+  button {
+    border-style: groove;
+  }
+  .shadow {
+    box-shadow: 5px 10px 10px rgba(0,0,0, 0.03);
   }
 </style>
--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>header</div>
+  <header>
+    <h1>Todo it!</h1>
+  </header>
 </template>
 <script>
 export default {
@@ -15,5 +17,9 @@ export default {
 };
 </script>
 <style>
-
+  h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+  }
 </style>
